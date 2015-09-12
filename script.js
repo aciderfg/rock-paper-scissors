26ddeb84c9cacbe171fbe7b9fb96ac81163237d9
aciderfg <acider.fg@gmail.com>
2015-09-12 13:10:58 -0500

    result messages now shows correctly

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ var humano = prompt("Que eliges Piedra Papel o Tijera");
 var computadora = Math.random();
 var resultado;
 
+
+
+
 //logica básica
-var generarRespuestas = function() {
 	if (computadora <= 0.33) {
 		computadora = "Piedra"
 	} else if (computadora > 0.33 && computadora <= 0.66) {
@@ -12,47 +14,49 @@ var generarRespuestas = function() {
 	} else if (computadora > 0.66) {
 		computadora = "Tijera"
 	}
-}
 
-var compararResultados = function(resultado1, resultado2) {
 
-		if (resultado1 == resultado2) {
-			resultado = "Empate";
-		} 
-		else if (resultado1 == "Piedra") {
-			if (resultado2 == "Papel") {
-				console.log( "perdiste");
-			}
-			else if (resultado2 == "Tijera") {
-				console.log( "ganaste");
-				resultado = "Ganaste";
-			}
+
+
+
+// The maths
+	if (humano == computadora) {
+		resultado = "Empate";
+	} 
+	else if (humano == "Piedra") {
+		if (computadora == "Papel") {
+			resultado == "perdiste";
 		}
-		else if (resultado1 == "Papel") {
-			if (resultado2 == "Piedra") {
-				resultado = "Ganaste";
-			}
-			else if (resultado2 == "Tijera") {
-				resultado = "Perdiste";
-			}
+		else if (computadora == "Tijera") {
+			resultado = "Ganaste";
 		}
-		else if (resultado1 == "Tijera") {
-			if (resultado2 == "Piedra") {
-				resultado = "Perdiste";
-			}
-			else if (resultado2 == "Papel") {
-				resultado = "Ganaste";
-			}
+	}
+	else if (humano == "Papel") {
+		if (computadora == "Piedra") {
+			resultado = "Ganaste";
 		}
+		else if (computadora == "Tijera") {
+			resultado = "Perdiste";
+		}
+	}
+	else if (humano == "Tijera") {
+		if (computadora == "Piedra") {
+			resultado = "Perdiste";
+		}
+		else if (computadora == "Papel") {
+			resultado = "Ganaste";
+		}
+	}
 
-}
 
-generarRespuestas();
-compararResultados(humano, computadora);
 
-console.log(humano);
-console.log(computadora);
-console.log(resultado);
+
+// The results
+	console.log(humano);
+	console.log(computadora);
+	console.log(resultado);
+
+
 
 
 //mostrar resultados
